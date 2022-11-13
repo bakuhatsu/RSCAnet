bc0647a62684a016affd392bf72b71d422162247
bakuhatsu <desertcoelacanth@gmail.com>
2022-11-13 01:50:11 -0500

    Added model

--- training.py
+++ training.py
@@ -29,10 +29,17 @@ from glob import glob
 from PIL import Image
 import matplotlib.pyplot as plt
 import copy
+# Possible additional imports (delete unneeded later)
+# import torchvision as tv
+# import torchvision.transforms as transforms
+# import torchvision.models as models
+# import torch.optim as optim
+import pretrainedmodels
+import numpy as np
 
 
 def parameters():
-    parser = argparse.ArgumentParser(description="SPCNET Trainer")
+    parser = argparse.ArgumentParser(description="RSCAnet Trainer")
     # Run quick-test mode or full training
     parser.add_argument("--quick_test", help="Runs only a single batch of train, val, and test for testing code,", action="store_true")
     args, args_other = parser.parse_known_args()
@@ -86,21 +93,177 @@ class ProduceDataset(Dataset):
         return (img_CHW, label)
 
 
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride = 1, downsample = None):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = nn.Sequential(
+                        nn.Conv2d(in_channels, out_channels, kernel_size = 3, stride = stride, padding = 1),
+                        nn.BatchNorm2d(out_channels),
+                        nn.ReLU())
+        self.conv2 = nn.Sequential(
+                        nn.Conv2d(out_channels, out_channels, kernel_size = 3, stride = 1, padding = 1),
+                        nn.BatchNorm2d(out_channels))
+        self.downsample = downsample
+        self.relu = nn.ReLU()
+        self.out_channels = out_channels
+        
+    def forward(self, x):
+        residual = x
+        out = self.conv1(x)
+        out = self.conv2(out)
+        if self.downsample:
+            residual = self.downsample(x)
+        out += residual
+        out = self.relu(out)
+        return out
+
+
+class LCAU_Block(nn.Module):
+  def __init__(self, in_channels, out_channels, l=5):
+    super(LCAU_Block, self).__init__()
+    ## Fill in the rest here
+    self.r = 2 # Set scale factor here
+    self.l = l # sqrt of 25 = 5
+    self.conv1 = nn.Conv2d(in_channels, l**2, kernel_size = 3, stride = 1, padding = 1)
+    self.nn_upsample = nn.Upsample(scale_factor=self.r, mode="nearest")
+    #self.batch_size = batch_size
+    self.conv2 = nn.Conv2d(4*in_channels, out_channels, kernel_size = 1)
+
+  def forward(self, x):
+    original = x
+    # Get dimensions for the passed tensor to use in steps below
+    batch_dim, channel_dim, H, W = x.size()
+    print("lcau1: ", x.shape)
+    out = self.conv1(x)
+    out = self.nn_upsample(out)
+    print("lcau2: ", out.shape)
+    # custom kernel for each pixel (values are pixel values for each channel l^2)
+    # Apply softmax to kernel (should apply independently for each kernel for best results, I think)
+    out = F.softmax(out, dim=1) # Should this get moved later? 
+
+    # Need to generate weights from out (reshape, view, and/or stack)
+    out = out.unfold(2, self.r, step=self.r)
+    out = out.unfold(3, self.r, step=self.r)
+    #out = out.reshape(batch_num, kernel_size ** 2, H, W, self.r ** 2) ## NEED TO FIX (variables don't exist)
+    out = out.reshape(batch_dim, self.l ** 2, H, W, self.r ** 2)
+    out = out.permute(0, 2, 3, 1, 4)
+    print("lcau3: ", out.shape)
+
+    # Reassembly
+    original = F.pad(original, pad=(self.l//2, self.l//2, self.l//2, self.l//2), 
+                     mode="constant", value=0)
+    # Put original tensor into right shape to multiply with weights tensor generated above
+    original = original.unfold(2, self.l, step=1)
+    original = original.unfold(3, self.l, step=1)
+    original = original.reshape(batch_dim, channel_dim, H, W, -1)
+    original = original.permute(0,2,3,1,4)
+    print("lcau4: ", out.shape)
+
+    # Perform multiplication to apply wieghts kernel to each pixel of original
+    out = torch.matmul(original, out)
+    out = out.reshape(batch_dim, H, W, -1)
+    out = out.permute(0, 3, 1, 2)
+    print("lcau5: ", out.shape)
+    out = self.nn_upsample(out)
+    print("lcau6: ", out.shape) # lcau6:  torch.Size([1, 256, 112, 112])
+    out = self.conv2(out)
+    print("lcau7: ", out.shape)
+
+    ############################
+    # Apply that kernel to each pixel of original in an upsampling operation
+    # original = self.nn_upsample(original) # This is equivalent to multiplying each pixel by all values of the kernel to upsample
+    # out = F.conv2d(original, weights, stride=1, padding=2, groups=self.batch_size)
+    # out = out.view(N, 15, 24, 24) # Not sure if I need to reshape this here?  
+    ############################
+    # Can upsample by 2, use nearest neighbor
+    # Return output (now upsampled by r (in this case 2x)) from original 
+    return out
+
+
+class RSCA_Resnet(nn.Module):
+    def __init__(self, block, layers, num_classes = 1000):
+        super(RSCA_Resnet, self).__init__()
+        self.model =  pretrainedmodels.__dict__['resnet18'](pretrained='imagenet')
+        self.inplanes = 64
+        self.conv1 = nn.Sequential(
+                        nn.Conv2d(3, 64, kernel_size = 7, stride = 2, padding = 3),
+                        nn.BatchNorm2d(64),
+                        nn.ReLU())
+        self.maxpool = nn.MaxPool2d(kernel_size = 3, stride = 2, padding = 1)
+        self.layer1 = self._make_layer(block, 64, layers[0], stride = 1)
+        self.layer2 = self._make_layer(block, 128, layers[1], stride = 2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride = 2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride = 2)
+        self.avgpool = nn.AvgPool2d(7, stride=1)
+        self.fc = nn.Linear(512, num_classes)
+        # LCAU_Block takes in_channels, out_channels
+        self.lcau1 = LCAU_Block(512, 256)  # 512/2 = 256
+        self.lcau2 = LCAU_Block(256, 128)  # 256/2 = 128
+        self.lcau3 = LCAU_Block(128, 64)   # 128/2 = 64
+        self.lcau4 = LCAU_Block(64, 32)    # 64/2 = 32
+    
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes:
+            
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes, kernel_size=1, stride=stride),
+                nn.BatchNorm2d(planes),
+            )
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+    
+    def forward(self, x: torch.Tensor): #-> List[torch.Tensor]:
+        # See note [TorchScript super()]
+        x = self.conv1(x)
+        #x = self.bn1(x)
+        #x0 = self.relu(x)
+        x = self.maxpool(x)
+
+        x1 = self.layer1(x)
+        x2 = self.layer2(x1)
+        x3 = self.layer3(x2)
+        x4 = self.layer4(x3)
+
+        #return [x1,x2,x3,x4] # returns features with channels [64,64,128,256,512]
+
+        ## Feature Pyramid with LCAU blocks
+        print("step1: ", x4.shape)
+        x = self.lcau1(x4)
+        print("step2: ", x.shape)
+        print("shape of x3: ", x3.shape)
+        # Sum x and x3
+        x += x3
+        print("step3: ", x.shape)
+        x = self.lcau2(x)
+        print("step4: ", x.shape)
+        x += x2
+        print("step5: ", x.shape)
+        x = self.lcau3(x)
+        print("step6: ", x.shape)
+        x += x1
+        print("step7: ", x.shape)
+        x = self.lcau4(x)
+        print("step8: ", x.shape)
+        # do LCAU steps take 2 inputs or are there additions (I think multiplications?) between steps?
+        # Nope, just addition as above (I think) 
+        
+        return x
+
+
 # For pytorch lightning, everything goes inside of the network class
-class SPCNet(LightningModule):
+class RSCANet(LightningModule):
     def __init__(self, num_classes=262, bs=10, lr=1e-3, workers=4, data_dir="../data/"):
         super().__init__()
-        # Load resnet18 model (not pretrained)
-        resnet_model = resnet18(pretrained=False)
-        # Get number of features
-        num_ftrs = resnet_model.fc.in_features
-        # Change last layer such that it will have the desired number of classes
-        resnet_model.fc = nn.Linear(num_ftrs, num_classes)
-        # if model is not None:
-        #     self.net = resnet_model
-        # else:
-        #     self.net = model
-        self.net = resnet_model
+        # Load RSCA model with Resnet18 backbone
+        rsca_model = ResnetMod(ResidualBlock, [3, 4, 6, 3])
+        # Use rsca_model as network for training
+        self.net = rsca_model
         self.data_dir = data_dir
         # Set batch size based on value passed
         self.bs = bs
@@ -108,8 +271,8 @@ class SPCNet(LightningModule):
         self.lr = lr
         # Set number of workers based on value passed
         self.workers = workers
-        # Set criterion for loss to CrossEntropyLoss
-        self.criterion = nn.CrossEntropyLoss()
+        # Set criterion for loss to Binary Cross Entropy Loss
+        self.criterion = nn.BCELoss()
         # For plotting training vs validation loss
         self.running_train_loss = []
         self.running_val_loss = []
